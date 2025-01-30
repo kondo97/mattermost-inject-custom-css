7b2e7eb396d2a3192ae190bd3986c67a7a2fbbc1
kondo97 <kondo9523@gmail.com>
2025-01-30 16:09:19 +0900

    Exec npm run lint

--- webapp/src/types/mattermost-webapp/index.d.ts
+++ webapp/src/types/mattermost-webapp/index.d.ts
@@ -3,5 +3,6 @@
 
 export interface PluginRegistry {
     registerRootComponent(component: React.ElementType);
+
     // Add more if needed from https://developers.mattermost.com/extend/plugins/webapp/reference
 }
