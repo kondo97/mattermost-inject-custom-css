0089a99a08ae038287cc63dcf50865737c677bdc
kondo97 <kondo9523@gmail.com>
2025-01-30 15:40:32 +0900

    Add Custom RootComponent

---
+++ webapp/src/components/root_styles/index.tsx
@@ -0,0 +1,8 @@
+// Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
+// See LICENSE.txt for license information.
+
+import * as React from 'react';
+import './styles.css';
+
+const RootStyles: React.FC = () => null;
+export default RootStyles;
